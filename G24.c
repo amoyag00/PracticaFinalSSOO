c8acb0b517d3b1cd586b8ad4ef82987c4a76c267
slebra00 <slebra00@estudiantes.unileon.es>
2017-01-12 22:09:35 +0100

    Añadido el semaforo (Queda comprobar que funcione) (Si hay muchos commits lo siento)

--- G24.c
+++ G24.c
@@ -3,15 +3,20 @@
 #include <pthread.h>
 #include <unistd.h>
 #define TRUE 1
+#define MAX_COCHES 5
 
 pthread_t boxesWaitList[5]={0,0,0,0,0};
 pthread_mutex_t mutexBox1;
 pthread_mutex_t mutexBox2;
+
+
+pthread_cond_t condCircuito=PTHREAD_COND_INITIALIZER;
+pthread_mutex_t semaforoCochesEnCircuito;
+
 void *boxesActions(void *arg);
 int main(){
 
-
-
+	
 
 	pthread_t box_1,box_2;
 	pthread_attr_t attrBox1;
@@ -28,6 +33,25 @@ int main(){
 	pthread_join(box_1,NULL);
 	pthread_join(box_2,NULL);
 
+	pthread_mutex_init(&semaforoCochesEnCircuito,NULL);
+
+	//Numero de coches en el circuito
+	int cochesEnCircuito=0;
+	
+	//Tiene que colocarse donde anyadamos coches al circuito que es el main (Samuel)
+	pthread_mutex_lock(&semaforoCochesEnCircuito);
+
+	while(cochesEnCircuito>=MAX_COCHES){
+
+		pthread_cond_wait(&condCircuito,&semaforoCochesEnCircuito);
+
+	}
+	pthread_mutex_unlock(&semaforoCochesEnCircuito);
+
+	//Se hará un signal cuando un coches salga del circuito
+	
+	
+
 	return 0;
 }
 
@@ -42,3 +66,4 @@ void *boxesActions(void *arg){
 	//}
 	
 }
+
