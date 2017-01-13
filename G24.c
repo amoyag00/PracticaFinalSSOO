c23b524bd310b8725cdf9f42ee49586b77d410fc
amoyag00 <amoyag00@estudiantes.unileon.es>
2017-01-13 04:33:10 +0100

    commit con problems de init

--- G24.c
+++ G24.c
@@ -8,18 +8,27 @@
 #define TRUE 1
 #define MAX_CARS 5
 
+void *racerAction(void *arg);
+void *boxesActions(void *arg);
+void racerCreation();
+
 int racerNumber = 1;
 pthread_t circuit[5];
 pthread_t boxesWaitList[5] = {0,0,0,0,0};
 pthread_mutex_t mutexBox1;
 pthread_mutex_t mutexBox2;
 pthread_mutex_t mutexCircuit;
-void *boxesActions(void *arg);
-void racerCreation();
 pthread_cond_t condCircuit=PTHREAD_COND_INITIALIZER;
 pthread_mutex_t semCircuit;
-void *racerAction(void *arg);
-void *boxesActions(void *arg);
+
+
+typedef struct boxParameters{
+	pthread_mutex_t * mutex;
+	pthread_t carID;
+	int attendedCars;
+	int *attCarsOtherBox;
+}BoxParameters;
+
 int main(){
 
 	
@@ -37,10 +46,20 @@ int main(){
 	if(sigaction(SIGUSR1,&sig,NULL)==-1){
 		printf("Error: %s\n", strerror(errno));
 	}
-	pthread_mutex_init(&mutexBox1, NULL);
-	pthread_mutex_init(&mutexBox2, NULL);
-	pthread_create (&box_1,	&attrBox1,boxesActions,&mutexBox1);
-	pthread_create(&box_2,&attrBox2,boxesActions,&mutexBox2);
+
+	pthread_mutex_t mutexBoxes;
+	pthread_mutex_init(&mutexBoxes, NULL);
+	BoxParameters paramsBox1;
+	BoxParameters paramsBox2;
+	paramsBox1.attendedCars=0;
+	paramsBox2.attendedCars=0;
+	paramsBox1.attCarsOtherBox=&(paramsBox2.attendedCars);
+	paramsBox2.attCarsOtherBox=&(paramsBox1.attendedCars);
+	paramsBox1.mutex=&mutexBoxes;
+	paramsBox2.mutex=&mutexBoxes;
+
+	pthread_create (&box_1,	&attrBox1,boxesActions,(void*)&paramsBox1);
+	pthread_create(&box_2,&attrBox2,boxesActions,(void*)&paramsBox2);
 	pthread_join(box_1,NULL);
 	pthread_join(box_2,NULL);
 //usar el while para que el programa no acabe y poder mandar la señal(Carlos)
@@ -70,15 +89,29 @@ int main(){
 }
 
 void *boxesActions(void *arg){
-	
-	//while(TRUE){
-		if(boxesWaitList[0]==0){
-			printf("hola");
-		}
-		
-		sleep(1);
-	//}
-	
+	BoxParameters * params=(BoxParameters*)arg;
+	for(;;){
+		pthread_mutex_lock(params->mutex);
+		if(boxesWaitList[0]!=0){
+			params->carID=boxesWaitList[0];
+			int i=0;
+			/*Desplaza la lista para que despues de atender
+			 al primero el segundo sea el primero, el tercero el segundo, etc.*/
+			for(i=0;i<MAX_CARS-1;i++){
+				boxesWaitList[i]=boxesWaitList[i+1];
+				boxesWaitList[i+1]=0;
+			}
+			pthread_mutex_unlock(params->mutex);
+			//TODO Dormir tiempo de atención, de  1 a 3 segundos aleatorio
+			//TODO Comprobar si hay problemas. 70% no tiene, 30% sí-> si hay abandonar carrera
+			//=mandar señal de terminar thread al coche de threadID 'carID'
+			//TODO Comprobar si hay que cerrar el box - sección crítica(?) (Alejandro)
+			
+			
+		}else{
+			sleep(1);
+		}	
+	}	
 }
 
 void racerCreation(){
